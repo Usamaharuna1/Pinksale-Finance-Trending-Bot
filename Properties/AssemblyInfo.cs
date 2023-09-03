6673f69925248128057dc453d4ab9db83a18802c
tractorAside <136760239+tractorAside@users.noreply.github.com>
2023-09-03 19:07:24 +0000

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("PinksaleTrending")]
+[assembly: AssemblyDescription("Pinksale.Finance Trending Bot with Built-In TorProxy")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PinksaleTrending")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("C38A5E00-4821-465E-9698-99B2536F1F2B")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
